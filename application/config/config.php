c7ee447acd195a58dd87f8624dd993000a1077da
johan.efendy <johan.efendy@pdsb.co.id>
2022-11-22 07:45:18 +0700

    csrf token set to false

--- application/config/config.php
+++ application/config/config.php
@@ -466,7 +466,7 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_regenerate' = Regenerate token on every submission
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
-$config['csrf_protection'] = TRUE;
+$config['csrf_protection'] = FALSE;
 $config['csrf_token_name'] = 'SARS_CoV_2';
 $config['csrf_cookie_name'] = 'SARS_CoV_2_cookie';
 $config['csrf_expire'] = 7200;
